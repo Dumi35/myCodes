65442aad338ab530925d03815cd1263b6b159102
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2023-09-18 21:49:11 +0100

    react-big-calendar

--- react/event-scheduler/src/App.js
+++ react/event-scheduler/src/App.js
@@ -1,80 +1,18 @@
+import React from 'react';
 import './App.css';
-import {useState} from 'react'
-import Calendar from 'react-calendar'; 
-
+import MyCalendar from './components/Calendar';
 
 function App() {
-  
-  const [event, setEvent] = useState(false)
-  const [eventTitle, setEventTitle] = useState("")
-  const [eventDate, setEventDate] = useState("")
-  const [allEvents, setAllEvents] = useState([])
-
-  const [date, setDate] = useState(new Date())
-
-
-  function createNewEvent(){
-    setEvent(true)
-  }
-
-  function saveEvent(){
-    const newEvent={
-      eventTitle,
-      eventDate
-    }
-    setAllEvents((prevEvents) => [...prevEvents, newEvent]);
-    setEvent(false); 
-  }
-
-
   return (
     <div className="App">
-  
-      <div className='events'>
-        <button id="new" onClick={createNewEvent}>New</button>
-        
-        {event && (
-          <form>
-            <input placeholder='Event Title' name='Title' type='text' onChange={event => setEventTitle(event.target.value)}/>
-            <input name='Event Date' type='date' onChange={event => setEventDate(event.target.value)}/>
-            <button onClick={saveEvent}>Save</button>
-            <button onClick={()=>{setEvent(false); }}>Cancel</button>
-          </form>
-        )}
-
-        {allEvents.map((event, index) => (
-          <div key={index}>
-            <h3>{event.eventTitle}</h3>
-            <p>{event.eventDate}</p>
-          </div>
-        ))}
-      </div>
-      
-
-      {/* calendar */}
-      <div className='Calendar'> 
-        <div className="app">
-          
-          <div className="calendar-container">
-            <Calendar onChange={setDate} value={date}/>
-          </div>
-          <div className="text-center">
-              Selected date: {date.toDateString()}
-          </div>
-        </div>
-      </div>
-  
-
+      <header className="App-header">
+        <h1>My Calendar App</h1>
+      </header>
+      <main>
+        <MyCalendar /> {/* Use your calendar component here */}
+      </main>
     </div>
   );
 }
 
 export default App;
- 
-
-
-
-
-
-
-
---
+++ react/event-scheduler/src/components/Calendar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = () => {
+  const events = [
+    {
+      title: 'Meeting',
+      start: new Date(2023, 8, 18, 10, 0),
+      end: new Date(2023, 8, 18, 12, 0),
+    },
+    // Add more events here...
+  ];
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+
+export default MyCalendar;
